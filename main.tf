53802a927d8adc845b487dbe9556071c9bbb917b
ManSinhLee <man.sinh.lee@gmail.com>
2023-11-29 10:19:09 +0700

    Deploy AWS VPC using Terraform and GitHub Actions

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "vpc" {
   name = "dev-eastus-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  azs              = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   database_subnets = ["10.0.8.0/24", "10.0.9.0/24", "10.0.10.0/24"]
 
@@ -14,7 +14,7 @@ module "vpc" {
   single_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
